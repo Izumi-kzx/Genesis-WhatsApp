12c7989b8279c3ca51b039845b9f693008c1ba95
Izumi.kzx <153532713+Izumi-kzx@users.noreply.github.com>
2025-02-23 14:36:50 -0400

    Actualizar Descargas•mediafire.js

--- plugins/Descargas•mediafire.js
+++ plugins/Descargas•mediafire.js
@@ -12,19 +12,23 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
   try {
     const response = await fetch(`https://api.agungny.my.id/api/soundclouds?q=${encodeURIComponent(query)}`)
     const data = await response.json()
-    if (!data.status || !data.result || data.result.length === 0) return conn.reply(m.chat, `🚫 No se encontraron resultados para "${query}".`, m)
+    
+    if (!data.status || !data.result || data.result.length === 0) {
+      return conn.reply(m.chat, `🚫 No se encontraron resultados para "${query}".`, m)
+    }
     
     let txt = '*🎵 S E A R C H - S O U N D C L O U D*\n\n'
     data.result.forEach(track => {
       txt += `🎶 *Título*: ${track.title}\n`
       txt += `🔗 *Enlace*: ${track.url}\n\n---------------------------------------------------\n\n`
     })
+    
     await conn.reply(m.chat, txt.trim(), m)
   } catch (error) {
     console.error(error)
     conn.reply(m.chat, '❌ Hubo un error al procesar la solicitud.', m)
   }
 }
-handler.command = ['soundcloudsearchv2']
+handler.command = ['soundcloudsearchtg']
 
 export default handler
